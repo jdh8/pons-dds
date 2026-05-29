eea130e092d56b6b1b8f0615e75b6cf432c5a216
github-action-benchmark <github@users.noreply.github.com>
2026-05-29 21:00:03 +0000

    add Benchmark (cargo) benchmark result for a33484a0c483e1a1c3f212e2f86ba25f8da02401

---
+++ dev/bench/data.js
@@ -0,0 +1,50 @@
+window.BENCHMARK_DATA = {
+  "lastUpdate": 1780088403581,
+  "repoUrl": "https://github.com/jdh8/dds-rs",
+  "entries": {
+    "Benchmark": [
+      {
+        "commit": {
+          "author": {
+            "email": "chen.pang.he@jdh8.org",
+            "name": "Chen-Pang He",
+            "username": "jdh8"
+          },
+          "committer": {
+            "email": "chen.pang.he@jdh8.org",
+            "name": "Chen-Pang He",
+            "username": "jdh8"
+          },
+          "distinct": true,
+          "id": "a33484a0c483e1a1c3f212e2f86ba25f8da02401",
+          "message": "ci: add docs and release test jobs; fix clippy collapsible-if warnings",
+          "timestamp": "2026-05-30T04:53:11+08:00",
+          "tree_id": "59628e0b530ec74460ca4d8916f9be2a5b2991eb",
+          "url": "https://github.com/jdh8/dds-rs/commit/a33484a0c483e1a1c3f212e2f86ba25f8da02401"
+        },
+        "date": 1780088402516,
+        "tool": "cargo",
+        "benches": [
+          {
+            "name": "solve_deal",
+            "value": 96132222,
+            "range": "± 213526739",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "solve_deals/32",
+            "value": 3252421250,
+            "range": "± 49974117",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "solve_deals/200",
+            "value": 22546745557,
+            "range": "± 183765742",
+            "unit": "ns/iter"
+          }
+        ]
+      }
+    ]
+  }
+}
